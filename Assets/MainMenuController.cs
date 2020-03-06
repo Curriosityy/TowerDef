6f1b4470b32ab1c028681979b3c8789af8beea14
Curriosityy <patryk9595@gmail.com>
2020-03-06 14:56:05 +0100

    =added main menu

---
+++ Assets/MainMenuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+public class MainMenuController : MonoBehaviour
+{
+    public void OnEnter(TMP_Text data)
+    {
+        data.color = Color.red;
+    }
+    public void OnExit(TMP_Text data)
+    {
+        data.color = Color.red;
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    public void GoToLevel(string name)
+    {
+        SceneChanger.Instance.ChangeScreen(name, UnityEngine.SceneManagement.LoadSceneMode.Single);
+    }
+}
